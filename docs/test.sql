9477fc10990ca0d7851292083e0347f965d847aa
likebeta <ixxoo.me@gmail.com>
2013-07-12 22:05:57 +0800

    modify sql

--- docs/test.sql
+++ docs/test.sql
@@ -10,15 +10,15 @@ Target Server Type    : MYSQL
 Target Server Version : 50520
 File Encoding         : 65001
 
-Date: 2013-07-03 19:54:17
+Date: 2013-07-12 22:04:44
 */
 
 SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
--- Table structure for `commonsvrds`
+-- Table structure for `basesvrds`
 -- ----------------------------
-DROP TABLE IF EXISTS `commonsvrds`;
-CREATE TABLE `commonsvrds` (
+DROP TABLE IF EXISTS `basesvrds`;
+CREATE TABLE `basesvrds` (
   `version` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `adminsvrd` varchar(255) NOT NULL,
   `dbsvrd` varchar(255) NOT NULL,
@@ -56,18 +56,18 @@ CREATE TABLE `versions` (
   `so` varchar(255) NOT NULL,
   `gamesvrd` varchar(255) NOT NULL,
   `client` varchar(255) NOT NULL,
-  `commonsvrds_ver` int(11) unsigned NOT NULL,
+  `basesvrds_ver` int(11) unsigned NOT NULL,
   `time` datetime NOT NULL,
   `comment` text,
   PRIMARY KEY (`version`),
   KEY `type` (`type`),
-  KEY `commonsvrds_ver` (`commonsvrds_ver`),
+  KEY `basesvrds_ver` (`basesvrds_ver`),
   CONSTRAINT `versions_ibfk_1` FOREIGN KEY (`type`) REFERENCES `gameinfo` (`type`),
-  CONSTRAINT `versions_ibfk_2` FOREIGN KEY (`commonsvrds_ver`) REFERENCES `commonsvrds` (`version`)
+  CONSTRAINT `versions_ibfk_2` FOREIGN KEY (`basesvrds_ver`) REFERENCES `basesvrds` (`version`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- View structure for `gamevisioninfo`
 -- ----------------------------
 DROP VIEW IF EXISTS `gamevisioninfo`;
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `gamevisioninfo` AS select `v`.`version` AS `version`,`g`.`type` AS `type`,`g`.`name` AS `name`,`g`.`description` AS `description`,`v`.`so` AS `so`,`v`.`gamesvrd` AS `gamesvrd`,`v`.`client` AS `client`,`v`.`commonsvrds_ver` AS `commonsvrds_ver`,`c`.`adminsvrd` AS `adminsvrd`,`c`.`dbsvrd` AS `dbsvrd`,`c`.`friendsvrd` AS `friendsvrd`,`c`.`logsvrd` AS `logsvrd`,`c`.`propertysvrd` AS `propertysvrd`,`c`.`proxysvrd` AS `proxysvrd`,`c`.`roommngsvrd` AS `roommngsvrd`,`c`.`shopsvrd` AS `shopsvrd`,`c`.`statsvrd` AS `statsvrd`,`c`.`websvrd` AS `websvrd`,`v`.`time` AS `time`,`v`.`comment` AS `comment` from ((`versions` `v` join `gameinfo` `g`) join `commonsvrds` `c`) where ((`v`.`type` = `g`.`type`) and (`v`.`commonsvrds_ver` = `c`.`version`));
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `gamevisioninfo` AS select `v`.`version` AS `version`,`g`.`type` AS `type`,`g`.`name` AS `name`,`g`.`description` AS `description`,`v`.`so` AS `so`,`v`.`gamesvrd` AS `gamesvrd`,`v`.`client` AS `client`,`v`.`basesvrds_ver` AS `basesvrds_ver`,`c`.`adminsvrd` AS `adminsvrd`,`c`.`dbsvrd` AS `dbsvrd`,`c`.`friendsvrd` AS `friendsvrd`,`c`.`logsvrd` AS `logsvrd`,`c`.`propertysvrd` AS `propertysvrd`,`c`.`proxysvrd` AS `proxysvrd`,`c`.`roommngsvrd` AS `roommngsvrd`,`c`.`shopsvrd` AS `shopsvrd`,`c`.`statsvrd` AS `statsvrd`,`c`.`websvrd` AS `websvrd`,`v`.`time` AS `time`,`v`.`comment` AS `comment` from ((`versions` `v` join `gameinfo` `g`) join `basesvrds` `c`) where ((`v`.`type` = `g`.`type`) and (`v`.`basesvrds_ver` = `c`.`version`));
